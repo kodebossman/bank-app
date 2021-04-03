52668b99116836eeeaeda14f88c171d9a5d9932f
kudakwashe <kuda@nightsbridge.co.za>
2021-04-03 15:16:25 +0200

    Added a new repository for the banking app frontend

---
+++ src/app/account/deposit-account/deposit-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deposit-account',
+  templateUrl: './deposit-account.component.html',
+  styleUrls: ['./deposit-account.component.css']
+})
+export class DepositAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { DepositAccountComponent } from './account/deposit-account/deposit-account.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { CreateTransactionComponent } from './transactions/create-transaction/create-transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    CreateAccountComponent,
+    DepositAccountComponent,
+    TransactionsComponent,
+    CreateTransactionComponent
   ],
   imports: [
     BrowserModule,
